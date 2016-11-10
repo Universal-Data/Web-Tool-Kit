2c813a648e7863311b0fbd3f8caed74cbc8b0f27
Eduardo Sant' Anna Martins <eduardomartins1993@gmail.com>
2016-11-10 14:09:14 -0200

    Change sass compile configurations

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,42 +1,46 @@
 module.exports = function(grunt) {
-  // Project Configuration
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    sass: {
-	dist: {
-		files:{
-		   'scss/app.scss': 'css/app.css'
-		}
-	},
-    },
-    cssmin: {
-	css: {
-		files: [
-			{
-				src: 'css/app.css',
-				dest: 'min/app.min.css',
+	// Project Configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			options: {
+				loadPath: [
+					'scss/modules/',
+				],
 			},
-		],
-	},
-    },
-    watch:{
-	css: {
-    		files: [
-			'scss/*.scss',
-		],
-		tasks: [
-			'sass',
-			'cssmin',
-		],
-	},
-    },
-  });
+			dist: {
+				files:{
+					'css/app.css': 'scss/app.scss'
+				}
+			},
+		},
+		cssmin: {
+			css: {
+				files: [
+					{
+						src: 'css/app.css',
+						dest: 'min/app.min.css',
+					},
+				],
+			},
+		},
+		watch:{
+			css: {
+					files: [
+					'scss/*.scss',
+					'scss/modules/*.scss',
+				],
+				tasks: [
+					'sass',
+					'cssmin',
+				],
+			},
+		},
+	});
 
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-  
-  grunt.registerTask('default', ['watch']);
-
+	grunt.registerTask('default', ['watch']);
 };
