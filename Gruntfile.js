14ea04acd76f9f8a62cd262377e2da3b015df2b9
Eduardo Sant' Anna Martins <eduardomartins1993@gmail.com>
2016-12-28 19:37:38 -0200

    Default changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,47 @@
-module.exports = function(grunt) {
-	// Project Configuration
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		sass: {
-			options: {
-				loadPath: [
-					'scss/modules/',
-				],
-			},
-			dist: {
-				files:{
-					'css/app.css': 'scss/app.scss'
-				}
-			},
-		},
-		cssmin: {
-			css: {
-				files: [
-					{
-						src: 'css/app.css',
-						dest: 'min/app.min.css',
-					},
-				],
-			},
-		},
-		watch:{
-			css: {
-					files: [
-					'scss/*.scss',
-					'scss/modules/*.scss',
-				],
-				tasks: [
-					'sass',
-					'cssmin',
-				],
-			},
-		},
-	});
+module.exports = function (grunt) {
+    // Project Configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            options: {
+                loadPath: [
+                    'scss/modules/',
+                ],
+            },
+            dist: {
+                files: {
+                    'css/app.css': 'scss/app.scss'
+                }
+            },
+        },
+        cssmin: {
+            css: {
+                files: [
+                    {
+                        src: 'css/app.css',
+                        dest: 'min/app.min.css',
+                    },
+                ],
+            },
+        },
+        watch: {
+            css: {
+                files: [
+                    'scss/*.scss',
+                    'scss/**/*.scss',
+                ],
+                tasks: [
+                    'sass',
+                    'cssmin',
+                ],
+            },
+        },
+    });
 
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+    //grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default', ['watch']);
+    grunt.registerTask('default', ['watch']);
 };
